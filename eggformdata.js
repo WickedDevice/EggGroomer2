161e099be9f819a15b63017b3efa10569bd294e1
Victor Aprea <victor.aprea@wickeddevice.com>
2016-02-01 16:53:04 -0500

    more progress toward angularjs-ification

--- eggformdata.js
+++ eggformdata.js
@@ -12,12 +12,43 @@ module.exports = (function(){
             ret = str.slice(1);
         }
 
-        if(ret.length >= 3 && ret.slice(0,3) == "egg"){
-            ret = str.slice(3);
-        }
+        //if(ret.length >= 3 && ret.slice(0,3) == "egg"){
+        //    ret = str.slice(3);
+        //}
 
         return ret;
     }
+
+    var headerRowToObjectFieldName = {
+        "CC3000 MAC address": "macAddress",
+        "Open Sensors .io password": "openSensorsPassword",
+        "Shipped Firmware Version": "shippedFirmwareVersion",
+        "CO Serial Number": "coSerialNumber",
+        "CO Date Code": "coDateCode",
+        "CO Sensitivity": "coSensitivity",
+        "CO Sensor Zero Value": "coOffset",
+        "NO2 Sensor Serial Number": "no2SerialNumber",
+        "NO2 Sensor Date Code": "no2DateCode",
+        "NO2 Sensitivity": "no2Sensitivity",
+        "NO2 Sensor Zero Value": "no2Offset",
+        "O3 Sensor Serial Number": "o3SerialNumber",
+        "O3 Sensor Date Code": "o3DateCode",
+        "O3 Sensitivity": "o3Sensitivity",
+        "O3 Sensor Zero Value": "o3Offset",
+        "SO2 Sensor Serial Number": "so2SerialNumber",
+        "SO2 Sensor Date Code": "so2DateCode",
+        "SO2 Sensitivity": "so2Sensitivity",
+        "SO2 Sensor Zero Value": "so2Offset",
+        "Paritculate Sensor Zero Value": "pmOffset",
+        "Date Shipped to customer": "dateShipped",
+        "Customer Name": "customerName",
+        "Customer Email": "customerEmail",
+        "Customer Order Number": "customerOrderNumber",
+        "Customer Address": "customerAddress",
+        "SHT25 / Egg Serial Number": "openSensorsUsername",
+        "Temperature Offset": "temperatureOffset",
+        "Humidity Offset": "humidityOffset"
+    };
 
     function processRows(rows){
         //console.log("Found rows:", rows);
@@ -49,21 +80,55 @@ module.exports = (function(){
             }
 
             egg_serial_number = stripLeadingTickAndEgg(egg_serial_number);
+            if(!egg_serial_number || egg_serial_number == "") continue;
 
             for(var field in rows[i]){
                 if(!form_database[egg_serial_number]){
                     // need a new entry, seed it with empty arrays
                     form_database[egg_serial_number] = {};
                     for(var ffield in first_row){
-                        form_database[egg_serial_number][first_row[ffield]] = [];
+                        if(headerRowToObjectFieldName[first_row[ffield]]) {
+                            form_database[egg_serial_number][headerRowToObjectFieldName[first_row[ffield]]] = [];
+                        }
                     }
                 }
 
                 // add the field data to it, now that it must exist
                 if(first_row[field]) {
-                    form_database[egg_serial_number][first_row[field]].push(
-                        stripLeadingTickAndEgg(rows[i][field])
-                    );
+                    var objFieldName = headerRowToObjectFieldName[first_row[field]];
+                    if(objFieldName) {
+                        form_database[egg_serial_number][objFieldName].push(
+                            stripLeadingTickAndEgg(rows[i][field])
+                        );
+                    }
+                }
+            }
+        }
+    }
+
+    function flattenFormDatabase(){
+        var eggSerialNumbers = Object.keys(form_database);
+        var mappedHeaderFieldKeys = Object.keys(headerRowToObjectFieldName);
+        var mappedHeaderFields = [];
+        for(var kk = 0; kk < mappedHeaderFieldKeys.length; kk++){
+            mappedHeaderFields.push(headerRowToObjectFieldName[mappedHeaderFieldKeys[kk]]);
+        }
+
+        // traverse the object and inspect the array lengths
+        for(var ii = 0; ii < eggSerialNumbers.length; ii++){
+            for(var jj = 0; jj < mappedHeaderFields.length; jj++){
+                if(mappedHeaderFields[jj] == "openSensorsUsername" && form_database[eggSerialNumbers[ii]][mappedHeaderFields[jj]].length >= 1){
+                    // keep an openSensorsUsername no matter what
+                    form_database[eggSerialNumbers[ii]][mappedHeaderFields[jj]] =
+                        form_database[eggSerialNumbers[ii]][mappedHeaderFields[jj]][0];
+                }
+                else if(form_database[eggSerialNumbers[ii]][mappedHeaderFields[jj]].length == 1){
+                    // it's unreliable data if you have 0 or more than 1 distinct value
+                    form_database[eggSerialNumbers[ii]][mappedHeaderFields[jj]] =
+                        form_database[eggSerialNumbers[ii]][mappedHeaderFields[jj]][0];
+                }
+                else{
+                    delete form_database[eggSerialNumbers[ii]][mappedHeaderFields[jj]];
                 }
             }
         }
@@ -90,6 +155,7 @@ module.exports = (function(){
                 // throw error?
             }).then(function (rows, info) {
                 processRows(rows);
+                flattenFormDatabase();
                 return form_database;
             });
         });
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,26 +3,116 @@
 
     app.controller('EggsController', function(){
         var mv = this;
-        mv.serialPorts = attachedSerialPorts;
         mv.eggs = attachedEggs;
-        mv.formDatabase = formDatabase;
     });
 
     var attachedEggs = [
-        /*
+/*
         {
-            sensorType: "NO2 / CO",
+            active: true,
             serial: {
-                macAddress: "AB:CD:EF:01:23:45"
-                ...
+                macAddress: { value: "AB:CD:EF:01:23:45"}
+                //...
             },
             form: {
                 macAddress: "01:23:45:AB:CD:EF"
-                ...
+               // ...
             }
         }
-        */
+*/
     ];
-    var attachedSerialPorts = [];
-    var formDatabase = [];
+
+    var numSerialPortsProcessed = 0;
+
+    function findOrCreateAttachedEggFromForm(formData){
+        var foundEgg = false;
+        for(var ii = 0; ii < attachedEggs.length; ii++){
+            if(attachedEggs[ii].serial && (attachedEggs[ii].serial.eggSerialNumber.value == formData.openSensorsUsername)){
+                attachedEggs[ii].form = formData;
+                foundEgg = true;
+                return;
+            }
+            else if(attachedEggs[ii].form && (attachedEggs[ii].form.openSensorsUsername == formData.openSensorsUsername)){
+                attachedEggs[ii].form = formData;
+                foundEgg = true;
+                return;
+            }
+        }
+
+        if(!foundEgg){
+            attachedEggs.push({
+                active: false,
+                form: formData
+            });
+        }
+    }
+
+    function findOrCreateAttachedEggFromSerial(serialData){
+        var foundEgg = false;
+        for(var ii = 0; ii < attachedEggs.length; ii++){
+            if(attachedEggs[ii].form && (attachedEggs[ii].form.openSensorsUsername == serialData.eggSerialNumber.value)){
+                attachedEggs[ii].serial = serialData;
+                foundEgg = true;
+                if(numSerialPortsProcessed == 0){
+                    attachedEggs[ii].active = true;
+                }
+                return;
+            }
+            else if(attachedEggs[ii].serial && (attachedEggs[ii].serial.eggSerialNumber.value == serialData.eggSerialNumber.value)){
+                attachedEggs[ii].serial = serialData;
+                foundEgg = true;
+                if(numSerialPortsProcessed == 0){
+                    attachedEggs[ii].active = true;
+                }
+                return;
+            }
+        }
+
+        if(!foundEgg){
+            if(numSerialPortsProcessed == 0){
+                attachedEggs.push({
+                    active: true,
+                    serial: serialData
+                });
+            }
+            else{
+                attachedEggs.push({
+                    active: false,
+                    serial: serialData
+                });
+            }
+            numSerialPortsProcessed++;
+        }
+    }
+
+
+
+    $.getJSON('/serialports', function( meh ) {
+        numSerialPortsProcessed = 0;
+        setInterval(function(){
+            $.getJSON('/serialports/eggs', function(data){
+                // for each egg that you find in the results
+                // look for a corresponding serial entry
+                // and if you don't find one, create a new one
+                // then put the egg data into the serial field
+                var serialNumbers = Object.keys(data);
+                for(var ii = 0; ii < serialNumbers.length; ii++){
+                    findOrCreateAttachedEggFromSerial(data[serialNumbers[ii]]);
+                }
+            });
+        }, 1000);
+    });
+
+    $.getJSON('/googleform', function( data ) {
+        console.log(data);
+        // for each egg that you find in the results
+        // look for a corresponding serial entry
+        // and if you don't find one, create a new one
+        // then put the egg data into the form field
+        var serialNumbers = Object.keys(data);
+        for(var ii = 0; ii < serialNumbers.length; ii++){
+            findOrCreateAttachedEggFromForm(data[serialNumbers[ii]]);
+        }
+    });
+
 })();
