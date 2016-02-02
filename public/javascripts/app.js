2f5a49ff45c3675a52f5913b78893c822cf7e715
Victor Aprea <victor.aprea@wickeddevice.com>
2016-02-01 21:26:53 -0500

    cool, it's rendering the ng-repeat

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,118 +1,102 @@
 (function(){
     var app = angular.module('egg-groomer', [ ]);
 
-    app.controller('EggsController', function(){
+    app.controller('EggsController', ['$http', function($http){
         var mv = this;
-        mv.eggs = attachedEggs;
-    });
+        mv.eggs = [];
 
-    var attachedEggs = [
-/*
-        {
-            active: true,
-            serial: {
-                macAddress: { value: "AB:CD:EF:01:23:45"}
-                //...
-            },
-            form: {
-                macAddress: "01:23:45:AB:CD:EF"
-               // ...
-            }
-        }
-*/
-    ];
+        var numSerialPortsProcessed = 0;
 
-    var numSerialPortsProcessed = 0;
-
-    function findOrCreateAttachedEggFromForm(formData){
-        var foundEgg = false;
-        for(var ii = 0; ii < attachedEggs.length; ii++){
-            if(attachedEggs[ii].serial && (attachedEggs[ii].serial.eggSerialNumber.value == formData.openSensorsUsername)){
-                attachedEggs[ii].form = formData;
-                foundEgg = true;
-                return;
-            }
-            else if(attachedEggs[ii].form && (attachedEggs[ii].form.openSensorsUsername == formData.openSensorsUsername)){
-                attachedEggs[ii].form = formData;
-                foundEgg = true;
-                return;
-            }
-        }
-
-        if(!foundEgg){
-            attachedEggs.push({
-                active: false,
-                form: formData
-            });
-        }
-    }
-
-    function findOrCreateAttachedEggFromSerial(serialData){
-        var foundEgg = false;
-        for(var ii = 0; ii < attachedEggs.length; ii++){
-            if(attachedEggs[ii].form && (attachedEggs[ii].form.openSensorsUsername == serialData.eggSerialNumber.value)){
-                attachedEggs[ii].serial = serialData;
-                foundEgg = true;
-                if(numSerialPortsProcessed == 0){
-                    attachedEggs[ii].active = true;
+        function findOrCreateAttachedEggFromForm(formData){
+            var foundEgg = false;
+            for(var ii = 0; ii < mv.eggs.length; ii++){
+                if(mv.eggs[ii].serial && (mv.eggs[ii].serial.eggSerialNumber.value == formData.openSensorsUsername)){
+                    mv.eggs[ii].form = formData;
+                    foundEgg = true;
+                    return;
                 }
-                return;
-            }
-            else if(attachedEggs[ii].serial && (attachedEggs[ii].serial.eggSerialNumber.value == serialData.eggSerialNumber.value)){
-                attachedEggs[ii].serial = serialData;
-                foundEgg = true;
-                if(numSerialPortsProcessed == 0){
-                    attachedEggs[ii].active = true;
+                else if(mv.eggs[ii].form && (mv.eggs[ii].form.openSensorsUsername == formData.openSensorsUsername)){
+                    mv.eggs[ii].form = formData;
+                    foundEgg = true;
+                    return;
                 }
-                return;
             }
-        }
 
-        if(!foundEgg){
-            if(numSerialPortsProcessed == 0){
-                attachedEggs.push({
-                    active: true,
-                    serial: serialData
-                });
-            }
-            else{
-                attachedEggs.push({
+            if(!foundEgg){
+                mv.eggs.push({
                     active: false,
-                    serial: serialData
+                    form: formData
                 });
             }
-            numSerialPortsProcessed++;
         }
-    }
 
-
-
-    $.getJSON('/serialports', function( meh ) {
-        numSerialPortsProcessed = 0;
-        setInterval(function(){
-            $.getJSON('/serialports/eggs', function(data){
-                // for each egg that you find in the results
-                // look for a corresponding serial entry
-                // and if you don't find one, create a new one
-                // then put the egg data into the serial field
-                var serialNumbers = Object.keys(data);
-                for(var ii = 0; ii < serialNumbers.length; ii++){
-                    findOrCreateAttachedEggFromSerial(data[serialNumbers[ii]]);
+        function findOrCreateAttachedEggFromSerial(serialData){
+            var foundEgg = false;
+            for(var ii = 0; ii < mv.eggs.length; ii++){
+                if(mv.eggs[ii].form && (mv.eggs[ii].form.openSensorsUsername == serialData.eggSerialNumber.value)){
+                    mv.eggs[ii].serial = serialData;
+                    foundEgg = true;
+                    if(numSerialPortsProcessed == 0){
+                        mv.eggs[ii].active = true;
+                    }
+                    return;
                 }
-            });
-        }, 1000);
-    });
+                else if(mv.eggs[ii].serial && (mv.eggs[ii].serial.eggSerialNumber.value == serialData.eggSerialNumber.value)){
+                    mv.eggs[ii].serial = serialData;
+                    foundEgg = true;
+                    if(numSerialPortsProcessed == 0){
+                        mv.eggs[ii].active = true;
+                    }
+                    return;
+                }
+            }
 
-    $.getJSON('/googleform', function( data ) {
-        console.log(data);
-        // for each egg that you find in the results
-        // look for a corresponding serial entry
-        // and if you don't find one, create a new one
-        // then put the egg data into the form field
-        var serialNumbers = Object.keys(data);
-        for(var ii = 0; ii < serialNumbers.length; ii++){
-            findOrCreateAttachedEggFromForm(data[serialNumbers[ii]]);
+            if(!foundEgg){
+                if(numSerialPortsProcessed == 0){
+                    mv.eggs.push({
+                        active: true,
+                        serial: serialData
+                    });
+                }
+                else{
+                    mv.eggs.push({
+                        active: false,
+                        serial: serialData
+                    });
+                }
+                numSerialPortsProcessed++;
+            }
         }
-    });
+
+
+
+        $.getJSON('/serialports', function( meh ) {
+            numSerialPortsProcessed = 0;
+            setInterval(function(){
+                $http.get('/serialports/eggs').success(function(data){
+                    // for each egg that you find in the results
+                    // look for a corresponding serial entry
+                    // and if you don't find one, create a new one
+                    // then put the egg data into the serial field
+                    var serialNumbers = Object.keys(data);
+                    for(var ii = 0; ii < serialNumbers.length; ii++){
+                        findOrCreateAttachedEggFromSerial(data[serialNumbers[ii]]);
+                    }
+                });
+            }, 1000);
+        });
+
+        $http.get('/googleform').success(function( data ) {
+            console.log(data);
+            // for each egg that you find in the results
+            // look for a corresponding serial entry
+            // and if you don't find one, create a new one
+            // then put the egg data into the form field
+            var serialNumbers = Object.keys(data);
+            for(var ii = 0; ii < serialNumbers.length; ii++){
+                findOrCreateAttachedEggFromForm(data[serialNumbers[ii]]);
+            }
+        });
+    }]);
 
 })();
